0264d313321c1496384f73eac6430d985e73ddb3
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2025-01-09 23:06:07 -0300

    -

---
+++ src/API/Api.Auth/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public string Get()
+        {
+            return "hello - Api Auth";
+        }
+    }
+}
--- src/API/Api.Auth/Program.cs
+++ src/API/Api.Auth/Program.cs
@@ -1,4 +1,3 @@
-
 namespace Api.Auth
 {
     public class Program
@@ -10,16 +9,10 @@ namespace Api.Auth
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
 
             app.UseHttpsRedirection();
 
