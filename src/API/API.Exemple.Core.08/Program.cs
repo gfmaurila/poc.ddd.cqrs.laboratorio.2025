69a5ae7206446bbe454c627f1782147224445039
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2025-01-16 23:44:05 -0300

    -

--- src/API/API.Exemple.Core.08/Program.cs
+++ src/API/API.Exemple.Core.08/Program.cs
@@ -3,45 +3,44 @@ using API.Exemple.Core._08.Extensions;
 using Serilog;
 using System.Reflection;
 
-namespace API.Exemple.Core._08
+namespace API.Exemple.Core._08;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        builder.Services.AddConnections();
+        builder.Services.AddEndpointsApiExplorer();
+        //builder.Services.AddSwaggerGen();
+
+        // Swagger
+        builder.Services.AddSwaggerConfig(builder.Configuration);
+        builder.Services.UseAuthentication(builder.Configuration);
+
+        builder.Services.AddInfrastructureServices(builder.Configuration);
+
+        builder.Host.UseSerilog((context, config) =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            config.ReadFrom.Configuration(builder.Configuration);
+        });
 
-            // Add services to the container.
+        // Register MediatR
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
 
-            builder.Services.AddControllers();
-            builder.Services.AddConnections();
-            builder.Services.AddEndpointsApiExplorer();
-            //builder.Services.AddSwaggerGen();
+        var app = builder.Build();
 
-            // Swagger
-            builder.Services.AddSwaggerConfig(builder.Configuration);
-            builder.Services.UseAuthentication(builder.Configuration);
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
-            builder.Services.AddInfrastructureServices(builder.Configuration);
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
 
-            builder.Host.UseSerilog((context, config) =>
-            {
-                config.ReadFrom.Configuration(builder.Configuration);
-            });
-
-            // Register MediatR
-            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
-
-            var app = builder.Build();
-
-            app.UseSwagger();
-            app.UseSwaggerUI();
-
-            app.UseHttpsRedirection();
-            app.UseAuthorization();
-            app.MapControllers();
-
-            app.Run();
-        }
+        app.Run();
     }
 }
