7ef565461d6e3ffc68ed3a6dce61e3caf26f357c
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2025-01-17 10:48:36 -0300

    padão de retorno nos controllers

---
+++ src/API/API.Exemple.Core.08/Controllers/ExempleController.cs
@@ -0,0 +1,58 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Exemple.Core._08.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class ExempleController(ISender sender, ILogger<ExempleController> logger) : BaseController(logger)
+{
+    private readonly ISender _sender = sender;
+
+    [HttpGet("GetOK")]
+    public async Task<IActionResult> GetOK()
+    {
+        return Ok("Teste");
+    }
+
+    [HttpGet("BadRequest")]
+    public async Task<IActionResult> GetBadRequest()
+    {
+        return BadRequest("Teste");
+    }
+
+    [HttpGet("NoContent")]
+    public async Task<IActionResult> GetNoContent()
+    {
+        return NoContent();
+    }
+
+    //[HttpGet("HandleError")]
+    //public async Task<IActionResult> GetHandleError()
+    //{
+    //    if (id != command.BasketId)
+    //        return BadRequest("Basket ID in URL does not match the command.");
+
+    //    var result = await _sender.Send(command);
+    //    if (result.IsSuccess)
+    //        return NoContent();
+    //    else
+    //        return HandleError(result.Error);
+    //}
+
+    //[HttpGet("CreatedAtAction")]
+    //public async Task<IActionResult> GetCreatedAtAction()
+    //{
+    //    var result = await _sender.Send(command);
+    //    if (result.IsSuccess)
+    //        return CreatedAtAction(nameof(GetBasket), new { id = result.Value }, result.Value);
+    //    return HandleError(result.Error);
+    //}
+
+    [HttpGet("/test")]
+    public string Test()
+    {
+        return "hello";
+    }
+}
---
+++ src/Core/Common.Core.08/Domain/Interface/IDomainError.cs
@@ -0,0 +1,10 @@
+﻿using Common.Core._08.Domain.Errors;
+
+namespace Common.Core._08.Domain.Interface;
+
+public interface IDomainError
+{
+    string? ErrorMessage { get; init; }
+    ErrorType ErrorType { get; init; }
+    public List<string>? Errors { get; init; }
+}
