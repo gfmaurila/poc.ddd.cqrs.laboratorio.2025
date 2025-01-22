fc39a6705499336db6e47095c91de68b6b72653e
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2025-01-21 23:55:40 -0300

    Identity

---
+++ src/External/API.External.Identity/Database/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using API.External.Identity.Database.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.External.Identity.Database;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+
+    public DbSet<Message> Messages { get; set; }
+}
+
+
