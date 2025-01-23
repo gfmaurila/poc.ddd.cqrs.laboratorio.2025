6bea3b22b58146cc3462fc9b42e4cf9d537bcf67
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2025-01-22 22:46:59 -0300

    -

--- src/External/API.External.Identity/Database/Model/Message.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API.External.Identity.Database.Model;
-
-public class Message
-{
-    public int Id { get; set; }
-    public string Title { get; set; } // Título da mensagem
-    public string Content { get; set; } // Conteúdo da mensagem
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Data de criação
-}
