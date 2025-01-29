06ff74d185ceefd5caf1e87a8db015193d36b0c3
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2025-01-29 12:05:56 -0300

    -

--- src/API/API.Exemple.Core.08/Feature/Exemple/GetPaginate/GetPaginateExempleQuery.cs
+++ src/API/API.Exemple.Core.08/Feature/Exemple/GetPaginate/GetPaginateExempleQuery.cs
@@ -1,32 +1,11 @@
 ﻿using API.Exemple.Core._08.Feature.Domain.Exemple.Models;
 using Common.Core._08.Request;
-using Common.Core._08.Response;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Exemple.Core._08.Feature.Exemple.GetPaginate;
-
-//public record GetPaginateExempleQuery(int PageNumber = 1, int PageSize = 10) : IRequest<ApiResult<List<ExempleQueryModel>>>;
 
 public class GetPaginateExempleQuery : QueryRequestPaged<GetPaginateExempleQueryResult, ExempleQueryModel>
 {
     [FromQuery]
     public string? FiltroFirstName { get; set; } // Filtro opcional
 }
-
-
-
-public class GetPaginateExempleQueryResult : QueryResponsePaged<ExempleQueryModel>
-{
-    public string? Filtro { get; set; } // Filtro aplicado
-    public int QuantidadePaginas { get; set; } // Quantidade de páginas
-
-    public GetPaginateExempleQueryResult(int total, List<ExempleQueryModel> items, string? filtro, int quantidadePaginas)
-        : base(total, items)
-    {
-        Filtro = filtro;
-        QuantidadePaginas = quantidadePaginas;
-    }
-}
-
-
-
