b8d7c57157c3a4d25b9a49af25c0060f3dd8c333
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2025-01-30 17:29:14 -0300

    -

---
+++ src/API/API.Exemple.Core.08/Feature/Notification/Messaging/Events/NotificationEventConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Exemple1.Core._08.Feature.Notification.Messaging.Events;
+
+public static class NotificationEventConstants
+{
+    public const string EventExempleCreate = "event-notification.create";
+    public const string EventExempleUpdate = "event-notification.update";
+    public const string EventExempleDelete = "event-notification.delete";
+}
