d27d418fda75c407add3cac78c6888156b767824
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2025-02-03 18:12:57 -0300

    API.Customer.Core ini

---
+++ src/API/API.Customer.Core/Infrastructure/Integration/ExternalEmail/Service/IExternalEmailService.cs
@@ -0,0 +1,13 @@
+﻿using API.Customer.Core.Feature.External.ExternalEmail.Get;
+using API.Customer.Core.Feature.External.ExternalEmail.Get.Model;
+using API.Customer.Core.Infrastructure.Integration.ExternalEmail.Model;
+
+namespace API.Customer.Core.Infrastructure.Integration.ExternalEmail.Service;
+
+public interface IExternalEmailService
+{
+    Task<CreateSendResponse> SendMessageAsync(CreateSendRequest request);
+    Task<List<ListSendResponse>> GetListSendAsync();
+    Task<List<EmailQueryModel>> GetPaginatedItemsAsync(GetPaginateEmailQuery query);
+    Task<int> GetTotalCountAsync();
+}
